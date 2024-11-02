4616f5cbe6b960112653b7185eea582c54de0988
aiueo49 <ilikeski2007@yahoo.co.jp>
2024-11-03 00:41:17 +0900

    自動生成ファイルの設定変更

--- test/controllers/staticpages_controller_test.rb
+++ test/controllers/staticpages_controller_test.rb
@@ -1,8 +1,8 @@
 require "test_helper"
 
 class StaticpagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get top" do
-    get staticpages_top_url
-    assert_response :success
-  end
+  # test "should get top" do
+  #   get staticpages_top_url
+  #   assert_response :success
+  # end
 end
