66af9d7f5e9b08916d2dc27a34ee1de6f7179d5d
aiueo49 <ilikeski2007@yahoo.co.jp>
2024-11-03 17:31:28 +0900

    トップページ他、テンプレート導入

--- app/controllers/staticpages_controller.rb
+++ app/controllers/staticpages_controller.rb
@@ -1,4 +1,19 @@
 class StaticpagesController < ApplicationController
   def top
   end
+
+  def blank
+  end
+
+  def forms
+  end
+
+  def tabs
+  end
+
+  def calendar
+  end
+
+  def tables
+  end
 end
